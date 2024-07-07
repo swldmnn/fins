a16600fdd8968b748a76a707884e1d2bbfcdd565
steffen <steffen_waldmann@gmx.de>
2024-07-07 22:22:00 +0200

    Added MUI, Added FilterBar

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,14 @@
 import React, { FunctionComponent, useContext, useState } from 'react';
 import { FinRecord } from '../utils/ReportParser';
 import { AppContext } from '../app-context';
-import MultiSelection from './MultiSelection';
-import { getCategoryIds, getSourcesByCategories } from '../utils/categoryUtil';
+import FilterBar from './FilterBar';
 
 enum TransactionType {
     credit = "credit",
     debit = "debit"
 }
 
-interface FinRecordFilters {
+export interface FinRecordFilters {
     categories: string[],
     transferSources: string[],
     transactionTypes: TransactionType[],
@@ -30,32 +29,6 @@ const Card: FunctionComponent<CardProps> = (props) => {
 
     const { state, updateState } = useContext(AppContext)
     const [filters, updateFilters] = useState(props.filterPresets ?? { categories: [], transferSources: [], transactionTypes: [], amountLimit: 0 })
-
-    const onCategoryChange = (selectedCategories: string[]) => {
-        updateFilters(prevFilters => ({
-            ...prevFilters,
-            categories: selectedCategories,
-            transferSources: getSourcesByCategories(selectedCategories).filter(transferSource => prevFilters.transferSources.includes(transferSource))
-        }))
-    }
-
-    const onTransferSourceChange = (selectedTransferSources: string[]) => {
-        updateFilters(prevFilters => ({
-            ...prevFilters, transferSources: selectedTransferSources
-        }))
-    }
-
-    const onTransactionTypeChange = (selectedTransactionTypes: string[]) => {
-        updateFilters(prevFilters => ({
-            ...prevFilters, transactionTypes: selectedTransactionTypes.map(type => TransactionType[type as keyof typeof TransactionType])
-        }))
-    }
-
-    const onAmountLimitChange = (amountLimit: number) => {
-        updateFilters(prevFilters => ({
-            ...prevFilters, amountLimit
-        }))
-    }
 
     const matchesTransactionTypes = (finRecord: FinRecord, transactionTypes: TransactionType[]): boolean => {
         const transactionType = finRecord.amount >= 0
@@ -82,28 +55,7 @@ const Card: FunctionComponent<CardProps> = (props) => {
         <div className='CardHeader'>
             {props.title && <div><h1>{props.title}</h1></div>}
             {props.showFilters && <div className='CardFilters'>
-                <MultiSelection
-                    values={Object.values(TransactionType).map(type => type.toString()).filter(value => isNaN(Number(value)))}
-                    selectedValues={filters.transactionTypes.map(type => type.toString())}
-                    onChange={(selectedFilters) => onTransactionTypeChange(selectedFilters)}
-                    translateLabels={true} />
-
-                <MultiSelection
-                    values={getCategoryIds()}
-                    selectedValues={filters.categories}
-                    onChange={(selectedFilters) => onCategoryChange(selectedFilters)}
-                    translateLabels={true} />
-
-                <MultiSelection
-                    values={getSourcesByCategories(filters.categories)}
-                    selectedValues={filters.transferSources}
-                    onChange={(selectedFilters) => onTransferSourceChange(selectedFilters)} />
-
-                <input
-                    type='number' min={0}
-                    value={filters.amountLimit}
-                    onChange={(e) => onAmountLimitChange(Number(e.target.value))} />
-
+                <FilterBar filters={filters} updateFilters={updateFilters} />
             </div>}
         </div>
         {childrenWithFinRecoreds}
---
+++ src/components/FilterBar.tsx
@@ -0,0 +1,141 @@
+import React, { FunctionComponent } from 'react';
+import { getCategoryIds, getSourcesByCategories } from '../utils/categoryUtil';
+import { Checkbox, FormControl, InputLabel, ListItemText, MenuItem, OutlinedInput, Select, SelectChangeEvent, Slider, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { FinRecordFilters } from './Card';
+
+enum TransactionType {
+    credit = "credit",
+    debit = "debit"
+}
+
+interface FilterBarProps extends React.PropsWithChildren {
+    filters: FinRecordFilters
+    updateFilters: React.Dispatch<React.SetStateAction<FinRecordFilters>>
+}
+
+const FilterBar: FunctionComponent<FilterBarProps> = ({ filters, updateFilters }) => {
+
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+        PaperProps: {
+            style: {
+                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250,
+            },
+        },
+    }
+
+    const onCategoryChange = (event: SelectChangeEvent<string[]>) => {
+        const {
+            target: { value },
+        } = event;
+        const selectedCategories = typeof value === 'string' ? value.split(',') : value
+
+        updateFilters(prevFilters => ({
+            ...prevFilters,
+            categories: selectedCategories,
+            transferSources: getSourcesByCategories(selectedCategories).filter(transferSource => prevFilters.transferSources.includes(transferSource))
+        }))
+    }
+
+    const onTransferSourceChange = (event: SelectChangeEvent<string[]>) => {
+        const {
+            target: { value },
+        } = event;
+        const selectedTransferSources = typeof value === 'string' ? value.split(',') : value
+
+        updateFilters(prevFilters => ({
+            ...prevFilters, transferSources: selectedTransferSources
+        }))
+    }
+
+    const onTransactionTypeChange = (
+        event: React.MouseEvent<HTMLElement>,
+        selectedTransactionTypes: string[],
+    ) => {
+        updateFilters(prevFilters => ({
+            ...prevFilters, transactionTypes: selectedTransactionTypes.map(type => TransactionType[type as keyof typeof TransactionType])
+        }))
+    }
+
+    const onAmountLimitChange = (event: Event, newValue: number | number[]) => {
+
+        const amountLimit = typeof newValue === 'number' ? newValue : newValue[0]
+
+        updateFilters(prevFilters => ({
+            ...prevFilters, amountLimit
+        }))
+    }
+
+    const allCategories = getCategoryIds()
+    const allTransferSources = getSourcesByCategories(filters.categories)
+
+    return <div className="FilterBar">
+        <ToggleButtonGroup
+            value={filters.transactionTypes}
+            onChange={onTransactionTypeChange}
+            aria-label="credit/debit"
+        >
+            <ToggleButton value="credit" aria-label="credit">
+                +
+            </ToggleButton>
+            <ToggleButton value="debit" aria-label="debit">
+                -
+            </ToggleButton>
+        </ToggleButtonGroup>
+
+        <FormControl sx={{ m: 1, width: 200 }}>
+            <InputLabel id="filter-category-label">Category</InputLabel>
+            <Select
+                labelId="filter-category-select-label"
+                id="filter-category-select"
+                multiple
+                value={filters.categories}
+                onChange={onCategoryChange}
+                input={<OutlinedInput label="Tag" />}
+                renderValue={(selected) => selected.join(', ')}
+                MenuProps={MenuProps}
+            >
+                {allCategories.map((category) => (
+                    <MenuItem key={category} value={category}>
+                        <Checkbox checked={filters.categories.indexOf(category) > -1} />
+                        <ListItemText primary={category} />
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, width: 200 }}>
+            <InputLabel id="filter-transaction-source-label">Source</InputLabel>
+            <Select
+                labelId="filter-transaction-source-select-label"
+                id="filter-transaction-source-select"
+                multiple
+                value={filters.transferSources}
+                onChange={onTransferSourceChange}
+                input={<OutlinedInput label="Tag" />}
+                renderValue={(selected) => selected.join(', ')}
+                MenuProps={MenuProps}
+            >
+                {allTransferSources.map((transferSource) => (
+                    <MenuItem key={transferSource} value={transferSource}>
+                        <Checkbox checked={filters.transferSources.indexOf(transferSource) > -1} />
+                        <ListItemText primary={transferSource} />
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+
+
+        <Slider sx={{ width: 100 }}
+            getAriaLabel={() => 'Temperature range'}
+            value={filters.amountLimit}
+            onChange={onAmountLimitChange}
+            valueLabelDisplay="auto"
+            max={10000}
+        />
+    </div>
+}
+
+export default FilterBar;
